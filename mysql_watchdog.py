a747d4e2de0ad2c542dd5d7c163b44f309b9ee57
Tiago Caxias <tcx.taliesin@gmail.com>
2016-02-01 12:22:21 +0000

    Changed to multi-source replication

--- mysql_watchdog.py
+++ mysql_watchdog.py
@@ -10,11 +10,18 @@ from os import listdir as ls, stat, getpid
 from stat import S_ISSOCK as is_socket
 from sys import stderr
 from multiprocessing import Pool
+import argparse
 
-check_time = 1
-main_port = 3308
-max_time = 60
 sock_dir = '/var/run/mysqld/'
+main_port = 3308
+check_time = 1
+max_time = 60
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('-c','--check_time',default=1,type=int,dest='check_time')
+arg_parser.add_argument('-p','--main_port',default=3308,type=int,dest='main_port')
+arg_parser.add_argument('-m','--max_time',default=60,type=int,dest='max_time')
+arg_parser.add_argument('-s','--sock_dir',default='/var/run/mysqld/',dest='sock_dir')
 
 my_cnf = {
     'option_files' : '/root/.my.cnf',
@@ -45,41 +52,46 @@ def do_cur(cnx,sql,cur_rets_dict=False):
     try:
         cur = cnx.cursor(dictionary=cur_rets_dict)
         cur.execute(sql)
-        db_data=cur.fetchone()
+        db_data = cur.fetchall()
         cur.close()
         return db_data
     except Exception,e:
         p_err(e, '->', sql)
-        return False
+        return None
 
 def get_master_status(cnx):
     db_data=do_cur(cnx,"""select count(*)
         from information_schema.processlist where user='repl'""")
     if isinstance(db_data, type(None)):
         return 0
-    if db_data[0]>1:
+    if db_data[0][0]>1:
         return 'up 100%'
     return 'down'
 
 def get_galera_status(cnx):
-    db_data=do_cur(cnx,"""select variable_value
+    db_data = do_cur(cnx,"""select variable_value
         from information_schema.global_status
         where variable_name='wsrep_local_state'""")
     if isinstance(db_data, type(None)):
         return 0
-    if db_data[0]=='4':
+    if db_data[0][0]=='4':
         return 'up 100%'
     return 'down'
 
 def get_slave_status(cnx):
-#    db_data=do_cur(cnx,"show all slaves status")
-    db_data=do_cur(cnx,"show slave status",True)
+    db_data=do_cur(cnx,"show all slaves status",True)
+    if isinstance(db_data, type(None)):
+        db_data=do_cur(cnx,"show slave status",True)
     if isinstance(db_data, type(None)):
         return 0
-    if isinstance(db_data['Seconds_Behind_Master'],int):
-        lag=db_data['Seconds_Behind_Master']
-    else:
-        return 'down'
+    lag = 0
+    for master in db_data:
+        if isinstance(master['Seconds_Behind_Master'],int):
+            lag = max(lag,master['Seconds_Behind_Master'])
+        else:
+            return 'down'
+    if lag>0:
+        p_err(lag,"seconds behind master - our max is ",max_time)
     if lag<max_time:
         return 'up ' + str(int(lag_to_percent(lag))) + '%'
     return 'down'
@@ -90,8 +102,8 @@ def get_port(cnx):
     where variable_name='port';""")
     if isinstance(db_data, type(None)):
         return 0
-    if int(db_data[0])>0:
-        return int(db_data[0])
+    if int(db_data[0][0])>0:
+        return int(db_data[0][0])
     return 0
 
 def check_cycle(cnx):
@@ -117,6 +129,14 @@ def listen_tcp(port):
         p_err("Got port ",port,", starting to listen.")
     serversocket.listen(10)
     return serversocket
+
+def answer(connection,response='down'):
+    try:
+        connection.send(nl(response))
+    except:
+        p_err("connection went away before I had a chance to answer")
+    finally:
+        connection.close()
 
 def spawn_monitor(my_cnf,main_port=main_port):
     haproxy_str = 'down'
@@ -147,18 +167,23 @@ def spawn_monitor(my_cnf,main_port=main_port):
             if time()-ts > check_time:
                 haproxy_str=check_cycle(cnx)
                 if isinstance(haproxy_str,int):
-                    connection.send(nl('down'))
-                    connection.close()
+                    answer(connection)
                     break
                 ts = time()
-            connection.send(nl(haproxy_str))
-            connection.close()
+            answer(connection,response=haproxy_str)
             if haproxy_str!=old_haproxy_str:
                 p_err("Changed from",old_haproxy_str,"to",haproxy_str)
                 old_haproxy_str=haproxy_str
 
 if __name__ == '__main__':
     # MAIN
+    try:
+        args = arg_parser.parse_args()
+    except Exception,e:
+        quit("Error parsing args")
+    else:
+        p_err(args)
+
     try:
         sockets = map(lambda x : sock_dir + x, get_sockets())
         sockets[0]+'abc'
